b0ce8b7041ef5b3cc64a28ac9e31976e45efddb2
Ahmed Alaa <ahmedalaa21500@gmail.com>
2024-07-29 00:12:43 +0300

    code change

--- task_4.sql
+++ task_4.sql
@@ -11,7 +11,7 @@ SELECT
     COLUMN_KEY,
     EXTRA
 FROM 
-    information_schema.COLUMNS
+    INFORMATION_SCHEMA.COLUMNS
 WHERE 
     TABLE_SCHEMA = 'alx_book_store'
     AND TABLE_NAME = 'Books';
